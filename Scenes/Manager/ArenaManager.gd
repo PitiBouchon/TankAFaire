0961b97e5722d32e737f60faa12866a0d2ab1261
Julien DOCK <dock.julien@orange.fr>
2021-07-27 17:09:14 +0200

    half of a basic controler

--- Scenes/Manager/ArenaManager.gd
+++ Scenes/Manager/ArenaManager.gd
@@ -6,11 +6,11 @@ var TANK_SCENE := preload("res://Scenes/Tank/Tank.tscn")
 func inistanciateTank(tankOne : TankData, tankTwo : TankData):
 	var newTank : Tank = TANK_SCENE.instance()
 	add_child(newTank)
-	newTank.loadData(tankOne)
+	newTank.loadData(tankOne, 1)
 	newTank.translation = Vector3(10, 0, 0)
 	
 	newTank = TANK_SCENE.instance()
 	add_child(newTank)
-	newTank.loadData(tankTwo)
+	newTank.loadData(tankTwo, 2)
 	newTank.translation = Vector3(-10, 0, 0)
 	pass
--- Scenes/Tank/Tank.gd
+++ Scenes/Tank/Tank.gd
@@ -1,16 +1,19 @@
 extends Spatial
 class_name Tank
 
+var _playerNumber : int
+var _speed : float
 
-
-func loadData(data : TankData):
+func loadData(data : TankData, player : int) -> void:
+	_playerNumber = player
+	
 	$Chassi.mesh = data.chassi.chassiMesh
 	$Chassi.translation = data.chassi.chassiPos
 	$Chassi.scale = data.chassi.chassiScale
 	
-	$Track.mesh = data.track.trackMesh
-	$Track.translation = data.track.trackPos
-	$Track.scale = data.track.trackScale
+	$Chassi/Track.mesh = data.track.trackMesh
+	$Chassi/Track.translation = data.track.trackPos
+	$Chassi/Track.scale = data.track.trackScale
 	
 	$Turret.mesh = data.turret.turretMesh
 	$Turret.translation = data.turret.turretPos
@@ -19,5 +22,31 @@ func loadData(data : TankData):
 	$Turret/Canon.mesh = data.turret.canonMesh
 	$Turret/Canon.translation = data.turret.canonPos
 	$Turret/Canon.scale = data.turret.canonScale
+	
+	_speed = computeSpeed(data)
+	return
+
+func computeSpeed(data : TankData) -> float:
+	#need implementation
+	return 10.0
+
+
+func _process(delta):
+	var direction : Vector3 = Vector3.ZERO
+	if(_playerNumber == 1):
+		if Input.is_action_pressed("ui_right"):
+			direction.x += 1
+		if Input.is_action_pressed("ui_left"):
+			direction.x -= 1
+		if Input.is_action_pressed("ui_up"):
+			direction.z -= 1
+		if Input.is_action_pressed("ui_down"):
+			direction.z += 1
+	
+	direction = direction.normalized()
+	translate(direction * _speed * delta)
+	
+	$Chassi.rotation.y = direction.angle_to(Vector3.FORWARD)
+	
 	
 	pass
