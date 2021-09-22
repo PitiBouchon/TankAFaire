a641cb84b514b0d233a4afd7e8c4b7e47842e606
Julien DOCK <dock.julien@orange.fr>
2021-09-22 12:04:28 +0200

    display in tank selection (except stats)

--- Scenes/Menus/TankSelection/TankSelection.gd
+++ Scenes/Menus/TankSelection/TankSelection.gd
@@ -8,6 +8,11 @@ export (Array, Resource) var engineList
 export (Array, Resource) var trackList
 export (Array, Resource) var turretList
 
+onready var uiPlayer1 : UITankSelection = $UI/UIPlayer1
+onready var uiPlayer2 : UITankSelection = $UI/UIPlayer2
+onready var tankPlayer1 : DisplayTank = $Player1
+onready var tankPlayer2 : DisplayTank = $Player2
+
 var tankOne : TankData
 var tankTwo : TankData
 
@@ -28,12 +33,37 @@ func _ready():
 	tankTwo = TankData.new()
 	player1Ready = false
 	player2Ready = false
-	player1Indexes = [0,0,0,0]
+	player1Indexes = [0,0,0,0] # chassi, engine, tracks, turret
 	player2Indexes = [0,0,0,0]
 	nbChassi = chassiList.size()
 	nbEngine = engineList.size()
 	nbTracks = trackList.size()
 	nbTurret = turretList.size()
+	
+	updatePlayer1()
+	updatePlayer2()
+
+
+func updatePlayer1() -> void:
+	tankOne.chassi = chassiList[player1Indexes[0]]
+	tankOne.engine = engineList[player1Indexes[1]]
+	tankOne.track = trackList[player1Indexes[2]]
+	tankOne.turret = turretList[player1Indexes[3]]
+	
+	uiPlayer1.updateStats()
+	
+	tankPlayer1.updateDisplay(tankOne)
+	
+
+func updatePlayer2() -> void:
+	tankTwo.chassi = chassiList[player2Indexes[0]]
+	tankTwo.engine = engineList[player2Indexes[1]]
+	tankTwo.track = trackList[player2Indexes[2]]
+	tankTwo.turret = turretList[player2Indexes[3]]
+	
+	uiPlayer2.updateStats()
+	
+	tankPlayer2.updateDisplay(tankTwo)
 
 
 # place holders --------------------------
@@ -60,73 +90,117 @@ func devDefaultFill():
 # player 1 -------------------------------
 
 func _on_UIPlayer1_next_chassi():
-	pass # Replace with function body.
+	player1Indexes[0] = (player1Indexes[0] + 1) % nbChassi
+	updatePlayer1() 
 
 
 func _on_UIPlayer1_next_engine():
-	pass # Replace with function body.
+	player1Indexes[1] = (player1Indexes[1] + 1) % nbEngine
+	updatePlayer1()
 
 
 func _on_UIPlayer1_next_track():
-	pass # Replace with function body.
+	player1Indexes[2] = (player1Indexes[2] + 1) % nbTracks
+	updatePlayer1()
 
 
 func _on_UIPlayer1_next_turret():
-	pass # Replace with function body.
+	player1Indexes[3] = (player1Indexes[3] + 1) % nbTurret
+	updatePlayer1()
 
 
 func _on_UIPlayer1_player_ready():
-	pass # Replace with function body.
+	player1Ready = true
+	if player1Ready && player2Ready:
+		emit_signal("tank_selected", tankOne, tankTwo)
 
 
 func _on_UIPlayer1_previous_chassi():
-	pass # Replace with function body.
+	if player1Indexes[0] == 0:
+		player1Indexes[0] = nbChassi - 1
+	else:
+		player1Indexes[0] -= 1
+	updatePlayer1()
 
 
 func _on_UIPlayer1_previous_engine():
-	pass # Replace with function body.
+	if player1Indexes[1] == 0:
+		player1Indexes[1] = nbEngine - 1
+	else:
+		player1Indexes[1] -= 1
+	updatePlayer1()
 
 
 func _on_UIPlayer1_previous_track():
-	pass # Replace with function body.
+	if player1Indexes[2] == 0:
+		player1Indexes[2] = nbTracks - 1
+	else:
+		player1Indexes[2] -= 1
+	updatePlayer1()
 
 
 func _on_UIPlayer1_previous_turret():
-	pass # Replace with function body.
+	if player1Indexes[3] == 0:
+		player1Indexes[3] = nbTurret - 1
+	else:
+		player1Indexes[3] -= 1
+	updatePlayer1()
 
 # player 2 ----------------------------------
 
 func _on_UIPlayer2_next_chassi():
-	pass # Replace with function body.
+	player2Indexes[0] = (player2Indexes[0] + 1) % nbChassi
+	updatePlayer2()
 
 
 func _on_UIPlayer2_next_engine():
-	pass # Replace with function body.
+	player2Indexes[1] = (player2Indexes[1] + 1) % nbEngine
+	updatePlayer2()
 
 
 func _on_UIPlayer2_next_track():
-	pass # Replace with function body.
+	player2Indexes[2] = (player2Indexes[2] + 1) % nbTracks
+	updatePlayer2()
 
 
 func _on_UIPlayer2_next_turret():
-	pass # Replace with function body.
+	player2Indexes[3] = (player2Indexes[3] + 1) % nbTurret
+	updatePlayer2()
 
 
 func _on_UIPlayer2_player_ready():
-	pass # Replace with function body.
+	player2Ready = true
+	if player1Ready && player2Ready:
+		emit_signal("tank_selected", tankOne, tankTwo)
 
 
 func _on_UIPlayer2_previous_chassi():
-	pass # Replace with function body.
+	if player2Indexes[0] == 0:
+		player2Indexes[0] = nbChassi - 1
+	else:
+		player2Indexes[0] -= 1
+	updatePlayer2()
 
 
 func _on_UIPlayer2_previous_engine():
-	pass # Replace with function body.
+	if player2Indexes[1] == 0:
+		player2Indexes[1] = nbEngine - 1
+	else:
+		player2Indexes[1] -= 1
+	updatePlayer2()
 
 
 func _on_UIPlayer2_previous_track():
-	pass # Replace with function body.
+	if player2Indexes[2] == 0:
+		player2Indexes[2] = nbTracks - 1
+	else:
+		player2Indexes[2] -= 1
+	updatePlayer2()
 
 
 func _on_UIPlayer2_previous_turret():
-	pass # Replace with function body.
+	if player2Indexes[3] == 0:
+		player2Indexes[3] = nbTurret - 1
+	else:
+		player2Indexes[3] -= 1
+	updatePlayer2()
