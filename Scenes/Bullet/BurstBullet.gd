8a460cb23f470298afc38bdbd04a4766f7f95f12
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2021-10-18 22:12:36 +0200

    Les balles rafales fonctionnent mais ne suivent pas le déplacement du tank... à suivre

---
+++ Scenes/Bullet/BurstBullet.gd
@@ -0,0 +1,18 @@
+extends Bullet
+
+export(PackedScene) var BasicBullet
+
+export(float) var interBulletTime = 0.2
+
+export(int) var amount = 12
+
+
+func initBullet(pos : Vector3, dir : Vector3, playerNumber : int) -> void :
+	.initBullet(pos, dir, playerNumber)
+	for i in range(amount):
+		var bullet : Bullet = BasicBullet.instance()
+		get_tree().current_scene.add_child(bullet)
+		#TODO : Suivre le déplacement du char
+		bullet.initBullet(self.global_transform.origin, dir, playerNumber)
+		yield(get_tree().create_timer(interBulletTime), "timeout")
+	queue_free()
