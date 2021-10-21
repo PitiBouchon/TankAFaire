4e1528f7f4c524e481f58a482bb930e9eda2722f
GuillaumeCx <guillaume.caucheteux@hotmail.fr>
2021-10-21 18:40:39 +0200

    La mine est maintenant opérationnelle. Si personne ne pénètre dans le rayon d'activation, elle fait des dégâts au bout de son compte à rebours, sinon, elle détonne au bout d'un délai après que quelqu'un a pénétré la zone

--- Scenes/Bullet/Landmine.gd
+++ Scenes/Bullet/Landmine.gd
@@ -10,21 +10,28 @@ export(float) var delay
 
 var elapsedTime : float = 0
 
+var triggered : bool = false
+
 func initBullet(pos : Vector3, dir : Vector3, playerNumber : int) -> void :
 	.initBullet(pos, dir, playerNumber)
 	#scale = Vector3(detectionRad, detectionRad, detectionRad)
 	$CollisionShape.shape.radius = detectionRad
 
+
 func _process(delta):
 	elapsedTime += delta
-	if elapsedTime >= countdown :
-		print("Pffffffuit")
-		queue_free()
+	if !triggered  && elapsedTime >= countdown :
+		explode()
 
-
+# Faut gérer les délais
 func _on_Landmine_body_entered(body):
-	print("Sup'")
-	if body.has_method("damage")==true:
-		body.damage(5)
-		print("Kaboum !")
+	triggered = true
+	yield(get_tree().create_timer(delay), "timeout")
+	explode()
+
+func explode():
+	var overlaps = get_overlapping_bodies()
+	for target in overlaps:
+		if target.has_method("damage"):
+			target.damage(damage)
 	queue_free()
