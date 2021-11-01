92f817e51bc8a5eb5a932c89919b13d47fa6e484
BiscuitPrime <henri.nomico@gmail.com>
2021-11-02 00:38:43 +0100

    1ère map fonctionnelle avec les ressources pour en faire d'autres

--- Scenes/Manager/ArenaManager.gd
+++ Scenes/Manager/ArenaManager.gd
@@ -2,6 +2,10 @@ extends Spatial
 class_name ArenaManager
 
 var TANK_SCENE := preload("res://Scenes/Tank/Tank.tscn")
+
+var MAP := preload("res://Scenes/Arenas/Arena2.tscn")
+
+var active_map
 var tank1
 var tank2 
 
@@ -10,12 +14,18 @@ func inistanciateTank(tankOne : TankData, tankTwo : TankData) -> void:
 	tank1 = TANK_SCENE.instance() 
 	add_child(tank1)
 	tank1.loadData(tankOne, 1)
-	tank1.translation = Vector3(10, 0, 0)
 	
 	tank2 = TANK_SCENE.instance() 
 	add_child(tank2)
 	tank2.loadData(tankTwo, 2)
-	tank2.translation = Vector3(-10, 0, 0)
+	
+	active_map=MAP.instance()
+	add_child(active_map)
+	
+	
+	
+	tank1.translation = active_map.get_node("SpawnPoints/SpawnPoint1").translation
+	tank2.translation = active_map.get_node("SpawnPoints/SpawnPoint2").translation
 	return
 
 
--- Scenes/Tank/Tank.gd
+++ Scenes/Tank/Tank.gd
@@ -2,18 +2,42 @@ extends KinematicBody
 class_name Tank
 
 var _playerNumber : int
-var _speed : float
-var _target : Vector3
-var _current_time : float 
-var _reload_time :float = 1 #A METTRE DANS DATA DU CANON
-var _health : int = 5 #Je met à 5 pour le moment, à modif
 
+var _speed : float
+var _health : int
+var _target : Vector3
+
+# reaload
+var _reloadTimer : float 
+var _realoadCooldown:float
+
+#dash
+var _isDashing : bool
+var _dashSpeed : float
+var _dashDuration : float
+var _dashTimer : float
+var _dashCooldown : float
+var _dashDirection : Vector3
+
+#bullet
 var _muzzelOffset : Vector3
 var _projectile : PackedScene
 
 func loadData(data : TankData, player : int) -> void:
 	_playerNumber = player
-	_current_time = 0
+	
+	_speed = computeSpeed(data)
+	_health = computeHealth(data)
+	
+	_reloadTimer = 0
+	_realoadCooldown= data.turret.realoadTime
+	
+	_isDashing = false
+	_dashSpeed = data.engine.dashSpeed
+	_dashDuration = data.engine.dashDistance / _dashSpeed
+	_dashTimer = 0
+	_dashCooldown = data.engine.dashCooldown
+	
 	_muzzelOffset = data.turret.relativeMuzzlePosition
 	_projectile = data.turret.bulletScene
 	
@@ -21,6 +45,10 @@ func loadData(data : TankData, player : int) -> void:
 	self.set_collision_layer_bit(_playerNumber,true)
 	self.set_collision_mask_bit(_playerNumber, true)
 	
+	self.set_collision_layer_bit(0,true)
+	self.set_collision_mask_bit(0, true)
+	
+	#meshes
 	$Chassi.mesh = data.chassi.chassiMesh
 	$Chassi.translation = data.chassi.chassiPos
 	$Chassi.scale = data.chassi.chassiScale
@@ -37,19 +65,22 @@ func loadData(data : TankData, player : int) -> void:
 	$Turret/Canon.translation = data.turret.canonPos
 	$Turret/Canon.scale = data.turret.canonScale
 	
-	_speed = computeSpeed(data)
-	
-	
 	return
 
 func computeSpeed(data : TankData) -> float:
 	#need implementation
 	return 10.0
 
+func computeHealth(data : TankData) -> float:
+	return data.turret.healthPoints + data.chassi.healthPoints
+
+
+
 func _process(delta):
 	processChassi(delta)
 	processTurret(delta)
-	_current_time += delta
+	_reloadTimer += delta
+	_dashTimer += delta
 	_target = get_parent().getTankPositionByID(3-_playerNumber)
 	pass
 
@@ -77,16 +108,35 @@ func processChassi(delta) -> void:
 			direction.z += 1
 	
 	direction = direction.normalized()
-	translate(direction * _speed * delta)
+	
+	if _playerNumber == 1 :
+		if Input.is_action_just_pressed("player1_dash") && !_isDashing && _dashTimer > _dashCooldown:
+			_isDashing = true
+			_dashTimer = 0
+			_dashDirection = direction
+	
+	if _playerNumber == 2 :
+		if Input.is_action_just_pressed("player2_dash") && !_isDashing && _dashTimer > _dashCooldown:
+			_isDashing = true
+			_dashTimer = 0
+			_dashDirection = direction
+	
+	if _isDashing :
+		processDash()
+	else:
+		move_and_slide(direction * _speed)
 	
 	$Chassi.rotation.y = acos(direction.z)
 	if direction.x != 0 :
 		$Chassi.rotation *= sign(direction.x)
 	return
 
-func updateTarget(pos : Vector3) -> void:
-	_target = pos
-	return
+func processDash() -> void:
+	if _dashTimer > _dashDuration :
+		_isDashing = false
+		return
+	move_and_slide(_dashDirection * _dashSpeed )
+	pass
 
 func processTurret(delta) -> void:
 	var direction : Vector3 = _target - translation
@@ -97,13 +147,13 @@ func processTurret(delta) -> void:
 	#On gère le tir des tanks :
 	if _playerNumber == 1:
 		if Input.is_action_pressed("player1_shoot"):
-			if _current_time > _reload_time :
-				_current_time=0 #Permet de gérer le reload
+			if _reloadTimer > _realoadCooldown:
+				_reloadTimer=0 #Permet de gérer le reload
 				shoot() 
 	if _playerNumber == 2:
 		if Input.is_action_pressed("player2_shoot"):
-			if _current_time > _reload_time :
-				_current_time=0
+			if _reloadTimer > _realoadCooldown:
+				_reloadTimer=0
 				shoot() 
 	return
 
